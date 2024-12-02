cceecf72e47dfb1f3d86dcad318e012633a0be7a
dabarshi <dabarshiroy@gmail.com>
2024-12-02 10:43:07 +0600

    update axiosinstance with dotenv

--- src/api/axiosInstance.jsx
+++ src/api/axiosInstance.jsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api", // Replace with your API base URL
+  baseURL: import.meta.env.VITE_BASE_URL, // Use Vite's environment variable syntax
   // baseURL: "http://localhost:5000/api", // Replace with your API base URL
   headers: {
     "Content-Type": "application/json",
